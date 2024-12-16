072ab3598ada308d9de68b010b6c990e1ee13f09
Jorge Vila <45121848+jorviltim@users.noreply.github.com>
2024-12-16 20:08:08 +0000

    [jorge vila] : proyecto  generador de dominios.

--- src/app.js
+++ src/app.js
@@ -9,3 +9,26 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let domine = [".com", ".org", ".es", ".net"];
+// declaro variable la cual voy a almacenar el texto a enviar
+let listMap = "";
+
+pronoun.map(itemPronoun => {
+  adj.map(itemAdj => {
+    noun.map(itemNoun => {
+      domine.map(itemDomine => {
+        listMap += ` 
+      <li class="list-group-item">
+      ${itemPronoun}${itemAdj}${itemNoun}${itemDomine}
+      </li>
+      `;
+      });
+    });
+  });
+});
+
+document.querySelector("#map").innerHTML = listMap;
